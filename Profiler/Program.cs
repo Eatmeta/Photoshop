f6b9d3c6c63b3fbd54425e4e45c4d4808b71bbe7
Eatmeta <metagroul@gmail.com>
2022-10-23 12:15:24 +0300

    Add Profiler project to compare performance with/without using reflection

---
+++ Profiler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using MyPhotoshop;
+using MyPhotoshop.Parameters;
+
+namespace Profiler
+{
+    class Program
+    {
+        static void Test(string description, Action<double[], LighteningParameters> action, int n)
+        {
+            var array = new double[] {0};
+            var obj = new LighteningParameters();
+            action(array, obj);
+            var watch = new Stopwatch();
+            watch.Start();
+            for (var i = 0; i < n; i++)
+            {
+                action(array, obj);
+            }
+            watch.Stop();
+            Console.WriteLine(description + ": " + 1000 * (double) watch.ElapsedMilliseconds / n + " ms");
+        }
+
+        static void Main(string[] args)
+        {
+            Test("Test with reflection", (values, pars) => pars.SetValues(values), 100_000);
+            Test("Test without reflection", (values, pars) => pars.Coefficient = values[0], 100_000);
+        }
+    }
+}
